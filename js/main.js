882913ae8586155f892d08abd11e8599043b82e0
Colin Stanford <j.colin.stanford@gmail.com>
2026-08-15 10:32:09 -0600

    fixed API url to match current documentation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   
-  const url = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters`;
+  const url = `https://api.hyrule-compendium.com/api/v3/compendium/category/monsters`;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
